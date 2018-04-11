b8450189c72f953ede744e70f6a404c3486106bd
Leonardo Bianconi <leonardo.bianconi@eldorado.org.br>
2018-04-11 12:26:03 -0300

    Added files for strncpy and strcpy tests
    - Added suport for testing strncpy and strcpy
    - Adeed gitignore file
    - Modified the main method to accept strncpy and strcpy

---
+++ StrcpyTest.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ITest.hpp"
+
+#include <cstddef>
+
+namespace funcperf {
+namespace string {
+
+class StrcpyTest : public funcperf::ITest
+{
+public:
+	StrcpyTest(int bufferSize, char* (*func)(char* dst, const char* src));
+	virtual ~StrcpyTest();
+
+	void run();
+
+private:
+	int m_bufferSize;
+	char* (*m_func)(char* dst, const char* src);
+	char* m_srcBuffer;
+	char* m_dstBuffer;
+};
+
+}
+}
--- Tester.cpp
+++ Tester.cpp
@@ -1,19 +1,38 @@
 #include "TestRunner.hpp"
 #include "MemcpyTest.hpp"
+#include "StrncpyTest.hpp"
+#include "StrcpyTest.hpp"
 
 #include <iostream>
 #include <cstring>
 
 #include <stdint.h>
 #include <dlfcn.h>
+#include <map>
+
+enum type {
+	memcpy_t,
+	strncpy_t,
+	strcpy_t
+};
 
 int main(int argc, char** argv)
 {
-	funcperf::TestRunner testRunner;
+        if (argc <= 1) {
+		std::cerr << "Error: Test type must be specified!" << std::endl;
+                std::cerr << "Available options: {memcpy; strncpy; strcpy}" << std::endl;
+		return 1;
+	}
+        std::string testType(argv[1]); 
+	std::map<std::string, type> typeMap; 
 
-	funcperf::string::MemcpyTest memcpyTestReference(10*1024*1024, &memcpy);
-	int64_t nanoDiffReference = testRunner.runTest(memcpyTestReference, 1000);
+	typeMap["memcpy"]	= memcpy_t;
+	typeMap["strncpy"]	= strncpy_t;
+	typeMap["strcpy"]      = strncpy_t;
 
+	std::cout << "Running tests using " << testType << "." << std::endl;
+
+	std::cout << "Loading candidate shared library...  ";
 	// load candidate shared library
 	void *soHandle = dlopen("/usr/obj/usr/src/powerpc.powerpc64/lib/libc/libc.so.7", RTLD_NOW);
 	if (soHandle == NULL) {
@@ -21,10 +40,37 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	void* (*candidateFunc)(void* dst, const void* src, size_t len) = (void* (*)(void*, const void*, size_t))dlfunc(soHandle, "memcpy");
+	std::cout << "OK."  << std::endl;
 
-	funcperf::string::MemcpyTest memcpyTestCandidate(10*1024*1024, candidateFunc);
-	int64_t nanoDiffCandidate = testRunner.runTest(memcpyTestCandidate, 1000);
+	funcperf::TestRunner testRunner;
+	funcperf::ITest* testReference;
+	funcperf::ITest* testCandidate;
+
+	std::cout << "Creating test objects... ";
+
+	switch (typeMap[testType]) {
+		case memcpy_t:
+			testReference = new funcperf::string::MemcpyTest(10*1024*1024, &memcpy);
+			testCandidate = new funcperf::string::MemcpyTest(10*1024*1024, (void* (*)(void*, const void*, size_t))dlfunc(soHandle, argv[1]));
+			break;
+		case strncpy_t:
+			testReference = new funcperf::string::StrncpyTest(10*1024*1024, &strncpy);
+			testCandidate = new funcperf::string::StrncpyTest(10*1024*1024, (char* (*)(char*, const char*, size_t))dlfunc(soHandle, argv[1]));
+			break;
+		case strcpy_t:
+			testReference = new funcperf::string::StrcpyTest(10*1024*1024, &strcpy);
+			testCandidate = new funcperf::string::StrcpyTest(10*1024*1024, (char* (*)(char*, const char*))dlfunc(soHandle, argv[1]));
+			break;
+	}
+
+	std::cout << "OK." << std::endl;
+
+	std::cout << "Running reference... ";
+	int64_t nanoDiffReference = testRunner.runTest(*testReference, 1000);
+	std::cout << "OK." << std::endl;
+	std::cout << "Running candidate... ";
+	int64_t nanoDiffCandidate = testRunner.runTest(*testCandidate, 1000);
+	std::cout << "OK." << std::endl;
 
 	std::cout << "Reference time: " << static_cast<double>(nanoDiffReference) / 1000000  << "ms." << std::endl;
 	std::cout << "Candidate time: " << static_cast<double>(nanoDiffCandidate) / 1000000  << "ms." << "(" << (100.0 * nanoDiffCandidate) / nanoDiffReference << "%)" << std::endl;
